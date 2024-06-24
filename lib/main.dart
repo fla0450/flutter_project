8474d16e02453d699e0619199f25ce13b9b6cae0
DongGun <fla27620450@gmail.com>
2024-06-24 14:34:59 +0900

    메인 페이지 수정

--- lib/main.dart
+++ lib/main.dart
@@ -30,121 +30,124 @@ class MyApp extends StatelessWidget {
   }
 }
 
-  class MyHomePage extends StatefulWidget {
-    const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-    final String title;
+  final String title;
 
-    @override
-    State<MyHomePage> createState() => _MyHomePageState();
-  }
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  class _MyHomePageState extends State<MyHomePage> {
-    final TextEditingController _idController = TextEditingController();
-    final TextEditingController _passwordController = TextEditingController();
-    String _errorMessage = '';
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String _errorMessage = '';
 
-    void _validateAndLogin() {
-      setState(() {
-        final id = _idController.text;
-        final password = _passwordController.text;
+  void _validateAndLogin() {
+    setState(() {
+      final id = _idController.text;
+      final password = _passwordController.text;
 
-        if (id.isEmpty || password.isEmpty) {
-          _errorMessage = '아이디와 비밀번호를 모두 입력하세요.';
-        } else {
-          _errorMessage = '';
-          if(id =="root1234" || password=="1234"){
-            MaterialPageRoute(
-              builder: (context) => Common_page(),
-            );
-          }
-        }
-      });
-    }
-
-    @override
-    void dispose() {
-      _idController.dispose();
-      _passwordController.dispose();
-      super.dispose();
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-              Text(
-                "MARKETINYOU",
-                style: TextStyle(
-                  fontSize: MediaQuery.of(context).size.width * 0.12,
-                  color: const Color(0xFFB22B15),
-                  fontFamily: "Black Han Sans",
-                ),
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-              TextField(
-                controller: _idController,
-                decoration: InputDecoration(
-                  fillColor: Colors.grey[200],
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: '아이디',
-                ),
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  fillColor: Colors.grey[200],
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: '비밀번호',
-                ),
-              ),
-              if (_errorMessage.isNotEmpty)
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Text(
-                    _errorMessage,
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-              InkWell(
-                borderRadius: BorderRadius.circular(30),
-                onTap: _validateAndLogin,
-                child: Container(
-                  width: 400,
-                  height: 50,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFB22B15),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: const Text(
-                    "로그인",
-                    style: TextStyle(color: Colors.white, fontSize: 25),
-                  ),
-                ),
-              ),
-            ],
+      if (id.isEmpty && password.isEmpty) {
+        _errorMessage = '아이디와 비밀번호를 모두 입력하세요.';
+      } else if (id == "root1234" && password == "1234") {
+        _errorMessage = '';
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Common_page(),
           ),
-        ),
-      );
-    }
+        );
+      } else {
+        _errorMessage = '아이디 또는 비밀번호가 잘못되었습니다.';
+      }
+    });
   }
+
+  @override
+  void dispose() {
+    _idController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+            Text(
+              "MARKETINYOU",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.12,
+                color: const Color(0xFFB22B15),
+                fontFamily: "Black Han Sans",
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+            TextField(
+              controller: _idController,
+              decoration: InputDecoration(
+                fillColor: Colors.grey[200],
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: '아이디',
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                fillColor: Colors.grey[200],
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: '비밀번호',
+              ),
+            ),
+            if (_errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  _errorMessage,
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            InkWell(
+              borderRadius: BorderRadius.circular(30),
+              onTap: _validateAndLogin,
+              child: Container(
+                width: 400,
+                height: 50,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFB22B15),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: const Text(
+                  "로그인",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 
 class Common_page extends StatelessWidget{
@@ -163,8 +166,12 @@ class Common_page extends StatelessWidget{
                 width: 0.95.sw,
                 height: 300,
                 decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(10.0)
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/main_img.jpg'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
               Container(
@@ -172,113 +179,60 @@ class Common_page extends StatelessWidget{
                 height: 100,
                 decoration: BoxDecoration(
                   color: Color(0xFFFECBCB),
-                  borderRadius: BorderRadius.circular(10.0)
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center, // 가운데 정렬
                   children: [
-                    SizedBox(width: MediaQuery.of(context).size.height *0.01),
                     InkWell(
-                        borderRadius: BorderRadius.circular(30),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Food_page()
-                              )
-                          );
-                          print("식사 터치");
-                        },
-                        child: Container(
-                          width: 0.2.sw,
-                          height: 50,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: const Color(0xFFB22B15),
-                              borderRadius: BorderRadius.circular(30)
+                      borderRadius: BorderRadius.circular(30),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Food_page(),
                           ),
-                          child: const Text(
-                            "식사류",
-                            style: TextStyle(color: Colors.white,fontSize: 20),
-                          ),
-                        )
+                        );
+                        print("식사 터치");
+                      },
+                      child: Container(
+                        width: 0.2.sw,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFB22B15),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Text(
+                          "식사류",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
                     ),
-                    SizedBox(width: MediaQuery.of(context).size.height *0.01),
+                    SizedBox(width: MediaQuery.of(context).size.height * 0.02), // 간격 조정
                     InkWell(
-                        borderRadius: BorderRadius.circular(30),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Shoppig_page()
-                              )
-                          );
-
-                        },
-                        child: Container(
-                          width: 0.2.sw,
-                          height: 50,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: const Color(0xFFB22B15),
-                              borderRadius: BorderRadius.circular(30)
+                      borderRadius: BorderRadius.circular(30),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Shoppig_page(),
                           ),
-                          child: const Text(
-                            "상점",
-                            style: TextStyle(color: Colors.white,fontSize: 20),
-                          ),
-                        )
-                    ),
-                    SizedBox(width: MediaQuery.of(context).size.height *0.01),
-                    InkWell(
-                        borderRadius: BorderRadius.circular(30),
-                        onTap: () {
-                          // Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //         builder: (context) => Common_page()
-                          //     )
-                          // );
-                          print("농산물 터치");
-                        },
-                        child: Container(
-                          width: 0.2.sw,
-                          height: 50,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: const Color(0xFFB22B15),
-                              borderRadius: BorderRadius.circular(30)
-                          ),
-                          child: const Text(
-                            "농산물",
-                            style: TextStyle(color: Colors.white,fontSize: 20),
-                          ),
-                        )
-                    ),
-                    SizedBox(width: MediaQuery.of(context).size.height *0.01),
-                    InkWell(
-                        borderRadius: BorderRadius.circular(30),
-                        onTap: () {
-                          // Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //         builder: (context) => Common_page()
-                          //     )
-                          // );
-                          print("농산물 터치");
-                        },
-                        child: Container(
-                          width: 0.2.sw,
-                          height: 50,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: const Color(0xFFB22B15),
-                              borderRadius: BorderRadius.circular(30)
-                          ),
-                          child: const Text(
-                            "침구류",
-                            style: TextStyle(color: Colors.white,fontSize: 20),
-                          ),
-                        )
+                        );
+                      },
+                      child: Container(
+                        width: 0.2.sw,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFB22B15),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Text(
+                          "상점",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
                     ),
                   ],
                 ),
